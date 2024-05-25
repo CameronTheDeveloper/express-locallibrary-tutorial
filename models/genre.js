41baa00b9ed66d8c78ba2c1caaf987de5f5feba7
CameronTheDeveloper <Cameronposey@outlook.com>
2024-05-25 14:19:30 -0700

    Fix genre detail route

--- models/genre.js
+++ models/genre.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-    name: {type: String, required: true, minLength: 3, maxLength: 100},
-})
+    name: { type: String, required: true, minLength: 3, maxLength: 100 },
+});
 
-GenreSchema.virtual('url').get(() => {
-    return `catalog/genre/${this._id}`
-})
+GenreSchema.virtual('url').get(function () {
+    return `genre/${this._id}`;
+});
 
 module.exports = mongoose.model("genre", GenreSchema);
