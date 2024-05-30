74844d7f0f01b54b725688b70d2ec702469100c4
Cameron Posey <cameronposey@outlook.com>
2024-05-30 07:46:33 -0700

    Add author_update GET data logic

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -142,7 +142,12 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author update GET");
+    const author = await Author.findById(req.params.id).exec();
+
+    res.render("author_form", {
+      title: "Update Author: ",
+      author: author,
+    })
 });
 
 // Handle Author update on POST.
