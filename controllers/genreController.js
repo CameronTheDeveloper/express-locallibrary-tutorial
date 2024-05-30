f4fa0d14a0eb2519ad07b69fe4b9948aade04439
Cameron Posey <cameronposey@outlook.com>
2024-05-30 09:03:38 -0700

    Add genre_update POST data logic

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -128,6 +128,31 @@ exports.genre_update_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+    
+    body("name", "Genre name must contain at least 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const genre = new Genre({
+            name: req.body.name,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()){
+            res.render("genre_form", {
+                title: "Update Genre: ",
+                genre: genre,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            const updatedGenre = await Genre.findByIdAndUpdate(req.params.id, genre, {});
+            res.redirect(updatedGenre.url);
+        }
+    }
+)];
