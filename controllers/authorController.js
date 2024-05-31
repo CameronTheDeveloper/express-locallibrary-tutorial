0ce8ccbb1d36ea9c461d182a1e2fe2266b443827
Cameron Posey <cameronposey@outlook.com>
2024-05-30 21:44:54 -0700

    Add author_update debug log

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,6 +2,7 @@ const Author = require("../models/author");
 const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
 const {body, validationResult} = require("express-validator");
+const debut = require("debug")("author");
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -143,6 +144,14 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
     const author = await Author.findById(req.params.id).exec();
+
+    if (author === null) {
+      // No results.
+      debug(`id not found on update: ${req.params.id}`);
+      const err = new Error("Author not found");
+      err.status = 404;
+      return next(err);
+    }
 
     res.render("author_form", {
       title: "Update Author: ",
